76241c993b35234997a2836d6c2592dc6ebe1a0c
AxelGanter <mail@axelganter.de>
2022-04-23 14:22:13 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div style="height: 100vh">
+    <Basic />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Basic from "./components/Basic.vue";
+</script>
+<style>
+@import "../node_modules/@braks/vue-flow/dist/style.css";
+@import "../node_modules/@braks/vue-flow/dist/theme-default.css";
+</style> 
